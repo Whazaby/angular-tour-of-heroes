1fddea588e81ed0b424673251cf201ed92f8effc
Guillaume Ruscassie <guillaume.ruscassie@gfi.fr>
2017-08-17 17:27:31 +0200

    Select a dashboard hero

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -10,18 +10,7 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-        <div *ngIf="hero">
-            <h2>{{hero.name}} details!</h2>
-            <div>
-                <label>id: </label>{{hero.id}}
-            </div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name"/>
-            </div>
-        </div>
-  `
+  templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit{
   @Input() hero: Hero;
@@ -34,5 +23,9 @@ export class HeroDetailComponent implements OnInit{
       this.route.paramMap
         .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
         .subscribe(hero => this.hero = hero);
+    }
+    
+    goBack(): void {
+        this.location.back();
     }
 }
